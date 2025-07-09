337adc5425a3d06ad1045019f9ce9e4eebdc3d99
jonaaix <jonaaix@icloud.com>
2025-07-09 14:26:03 +0200

    Release

--- projects/ngx-modern-alerts/src/public-api.ts
+++ projects/ngx-modern-alerts/src/public-api.ts
@@ -2,7 +2,16 @@
  * Public API Surface of ngx-modern-alerts
  */
 
-export * from './lib/components/ngx-modern-alert/ngx-modern-alert.component';
-export * from './lib/core/ngx-modern-alert';
-export * from './lib/ngx-modern-alert.module';
+// Services
 export * from './lib/services/ngx-modern-alert.service';
+
+// Components
+export * from './lib/components/ngx-modern-alert/ngx-modern-alert.component';
+export * from './lib/components/ngx-modern-alert-hub/ngx-modern-alert-hub.component';
+
+// Core
+export * from './lib/core/ngx-modern-alert';
+export * from './lib/core/ngx-modern-alert-enums';
+
+// Module
+export * from './lib/ngx-modern-alert.module';
